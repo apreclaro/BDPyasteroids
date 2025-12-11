5b0b1dee38836f3e8b9e1b3c6cadea5ad67595db
apreclaro <apreclaro@aL2n1.localdomain>
2025-12-10 21:25:08 -0800

    2:Balls added

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 import pygame
 import constants
+import player
 from logger import log_state
+import asteroid
+import asteroidfield
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    width = constants.SCREEN_WIDTH
+    height = constants.SCREEN_HEIGHT
+    radius = constants.PLAYER_RADIUS
+    asteroid.Asteroid.containers = (asteroids,updatable,drawable)
+    player.Player.containers = (updatable,drawable)
+    asteroidfield.AsteroidField.containers = (updatable)
+    asfield = asteroidfield.AsteroidField()
+    game_clock = pygame.time.Clock()
+    dt = 0
+    tri_player = player.Player(width/2,height/2,radius)
+    screen = pygame.display.set_mode((width,height))
     x=0
     y=0
     while x == y:
         log_state()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
         screen.fill("black")
+        updatable.update(dt)
+        for i in drawable: 
+            i.draw(screen)
         pygame.display.flip()
+        dt=game_clock.tick(60)/1000
+        
 
     
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}!")
---
+++ player.py
@@ -0,0 +1,47 @@
+import constants
+import circleshape
+import pygame
+
+class Player(circleshape.CircleShape):
+    
+    def __init__(self, x, y, z):
+        super().__init__(x,y,z)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        color = "white" 
+        points = self.triangle()
+        width = constants.LINE_WIDTH
+        pygame.draw.polygon(screen, color, points, width)
+
+    def rotate(self,dt):
+        self.rotation += constants.PLAYER_TURN_SPEED * dt
+    
+    def update(self,dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+            
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        
+        if keys[pygame.K_s]:
+            self.move(-dt)
+            
+        if keys[pygame.K_w]:
+            self.move(dt)
+    
+    def move(self,dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * constants.PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
